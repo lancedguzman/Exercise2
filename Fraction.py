319f1fbb1d047551cb1953c8607e4b4bb75e3a23
Lance B. De Guzman <137474979+lancedguzman@users.noreply.github.com>
2025-01-30 12:03:58 +0800

    Part A
    I instantiated part A.

---
+++ Fraction.py
@@ -0,0 +1,22 @@
+class Fraction(object):
+
+    def __init__(self, numerator=0, denominator=1):
+        self.get_numerator()
+        self.get_denominator()
+        pass
+
+    def gcd(a, b):
+        #TODO
+        pass
+
+    def get_numerator(self):
+        #TODO
+        pass
+
+    def get_denominator(self):
+        #TODO
+        pass
+
+    def get_fraction(self):
+        #TODO
+        pass
